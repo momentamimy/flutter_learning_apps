ce4cdfbfe2dc8b139924d5133e797d330c00d45d
EgyTech <momentamimy@gmail.com>
2023-09-19 12:20:14 +0300

    test

--- unit_testing_projects/validator_unit_testing/lib/main.dart
+++ unit_testing_projects/validator_unit_testing/lib/main.dart
@@ -40,7 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
